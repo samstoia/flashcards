a2469a1f2b8e6171830ac10336e51a53a0efb75c
Micaela Jawor and Sam Stoia <micaela@>
2019-01-15 16:37:51 -0800

    It will add more toggles in js and css

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,21 @@ $(document).ready(function(){
     $("#definition-js").toggle();
   });
 });
+
+$(document).ready(function(){
+  $(".panel-title-operators").click(function(){
+    $("#definition-operators").toggle();
+  });
+  $("#definition-operators").click(function(){
+    $("#definition-operators").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".panel-title-variables").click(function(){
+    $("#definition-variables").toggle();
+  });
+  $("#definition-variables").click(function(){
+    $("#definition-variables").toggle();
+  });
+});
