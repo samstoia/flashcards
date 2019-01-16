057a094749ed2a5bbd6d4afbbc77cf7d5b88a94b
Micaela Jawor and Sam Stoia <micaela@>
2019-01-15 16:29:35 -0800

    got toggle to workgit add .git add .

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".panel-title-js").click(function(){
+    $("#definition-js").toggle();
+  });
+  $("#definition-js").click(function(){
+    $("#definition-js").toggle();
+  });
+});
